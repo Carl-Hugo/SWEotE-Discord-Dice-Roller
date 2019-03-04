cbef5e2c899d141bcec09b7d4cd8e40aa3ab6825
Carl-Hugo Marcotte <carlhugo@gmail.com>
2019-03-03 22:24:40 -0500

    Update deploy scripts

--- deploy.ps1
+++ deploy.ps1
@@ -8,14 +8,8 @@ if ($tag -eq "auto") {
 }
 Write-Output "tag: $tag"
 
-az acr login --name fortheneworder
+docker login -u carlhugo
 
 docker build --rm -f "Dockerfile" -t my-d1-c3:$tag .
-docker tag my-d1-c3:$tag fortheneworder.azurecr.io/my-d1-c3:$tag
-docker push fortheneworder.azurecr.io/my-d1-c3:$tag
-
-az container create --resource-group for-the-new-order --name my-d1-c3-container --image fortheneworder.azurecr.io/my-d1-c3:$tag --dns-name-label my-d1-c3 --ports 80
-az container show --resource-group for-the-new-order --name my-d1-c3-container --query "{FQDN:ipAddress.fqdn,ProvisioningState:provisioningState}" --out table
-
-
-
+docker tag my-d1-c3:$tag carlhugo/my-d1-c3:$tag
+docker push carlhugo/my-d1-c3:$tag
